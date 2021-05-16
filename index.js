73ed695fbc4edffa162cd8a798fb51c1f46599cb
tharon <tharonweighill@gmail.com>
2021-05-16 17:27:17 -0600

    commit tree 04b4e29b7ae144609a3574b20f214a543164acdc

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const body = document.getElementsByTagName("body")[0]
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "BOO BOO is the champion" 
+body.appendChild(newHeader)
